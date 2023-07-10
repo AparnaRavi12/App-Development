0adde721ef7e320bd468c51b9af390e5387d3ff5
AparnaRavi12 <131737371+AparnaRavi12@users.noreply.github.com>
2023-07-10 21:40:13 +0530

    Add files via upload

---
+++ Day-3/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
